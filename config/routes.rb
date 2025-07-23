3be8eee340d409a364d9c5e71b8d74e68c49ca59
h-waji <goo.hiro.w@gmail.com>
2025-07-23 16:59:39 +0900

    rootをサインイン画面に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
   get "/:username", to: "profiles#show", as: :profile
 end
