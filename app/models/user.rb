009e616741998a415041de1d5b1c2ec929b7cb64
h-waji <goo.hiro.w@gmail.com>
2025-07-23 15:31:40 +0900

    DeviseのView一式を生成。ログインフォームをusernameに修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(
+        ["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]
+      ).first
+    else
+      where(conditions).first
+    end
+  end
 end
