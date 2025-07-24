3f69708a9664a535a82b971e603c1aad3ef10ba5
h-waji <goo.hiro.w@gmail.com>
2025-07-24 17:19:46 +0900

    プロフィール画面に最新の投稿を表示

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,10 @@
 class ProfilesController < ApplicationController
   def show
-    @user = User.find_by_username(params[:username])
+    @user = User.find_by(username: params[:username])
     unless @user
       render file: "#{Rails.root}/public/404.html", status: 404
     end
+
+    @latest_daily_post = @user.daily_posts.order(posted_on: :desc).first
   end
 end
