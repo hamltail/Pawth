432f69f03aa376292307b91d9e72b97359bd91c7
h-waji <goo.hiro.w@gmail.com>
2025-07-28 09:43:16 +0900

    プロフィールにカレンダー追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,5 +6,11 @@ class ProfilesController < ApplicationController
     end
 
     @latest_daily_post = @user.daily_posts.order(posted_on: :desc).first
+
+    today = Date.current
+    @calendar_days = (today.beginning_of_month..today.end_of_month).to_a
+    @daily_posts_by_date = @user.daily_posts
+                                .where(posted_on: today.beginning_of_month..today.end_of_month)
+                                .group_by(&:posted_on)
   end
 end
