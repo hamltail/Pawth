10efb04b6b786c14a357a36605be431f7b61d27c
h-waji <goo.hiro.w@gmail.com>
2025-07-28 12:44:39 +0900

    カレンダーJSをturbo:loadで再登録するよう修正

--- app/javascript/components/calendar.js
+++ app/javascript/components/calendar.js
@@ -1,8 +1,10 @@
-document.querySelectorAll('span[data-date]').forEach(el => {
-  el.addEventListener('click', function() {
-    const date = this.dataset.date;
-    const content = this.dataset.content;
-    document.getElementById('daily-post-date').textContent = date;
-    document.getElementById('daily-post-content').textContent = content || "まだ投稿がありません。";
+document.addEventListener("turbo:load", function() {
+  document.querySelectorAll('span[data-date]').forEach(el => {
+    el.addEventListener('click', function() {
+      const date = this.dataset.date;
+      const content = this.dataset.content;
+      document.getElementById('daily-post-date').textContent = date;
+      document.getElementById('daily-post-content').textContent = content || "まだ投稿がありません。";
+    });
   });
 });
