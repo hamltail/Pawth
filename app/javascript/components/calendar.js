070b35e226e645e2aa0496c83d04784bec046110
h-waji <goo.hiro.w@gmail.com>
2025-07-28 12:27:49 +0900

    カレンダークリックで投稿内容を表示

---
+++ app/javascript/components/calendar.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('span[data-date]').forEach(el => {
+  el.addEventListener('click', function() {
+    const date = this.dataset.date;
+    const content = this.dataset.content;
+    document.getElementById('daily-post-date').textContent = date;
+    document.getElementById('daily-post-content').textContent = content || "まだ投稿がありません。";
+  });
+});
