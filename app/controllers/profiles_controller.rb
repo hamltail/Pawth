414386a841ab01259abaa9ffd4fce29dede6cb68
h-waji <goo.hiro.w@gmail.com>
2025-07-28 13:06:03 +0900

    カレンダー月送り機能追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,10 +7,18 @@ class ProfilesController < ApplicationController
 
     @latest_daily_post = @user.daily_posts.order(posted_on: :desc).first
 
-    today = Date.current
+    today = if params[:year].present? && params[:month].present?
+      Date.new(params[:year].to_i, params[:month].to_i)
+    else
+      Date.current
+    end
+
     @calendar_days = (today.beginning_of_month..today.end_of_month).to_a
     @daily_posts_by_date = @user.daily_posts
                                 .where(posted_on: today.beginning_of_month..today.end_of_month)
                                 .group_by(&:posted_on)
+    @prev_month = today.prev_month
+    @next_month = today.next_month
+    @month_label = today.strftime("%B %Y")
   end
 end
