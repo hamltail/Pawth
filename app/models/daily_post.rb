598e6446886d407334d05eb698ad7ed30a3f04a0
h-waji <goo.hiro.w@gmail.com>
2025-07-29 11:05:10 +0900

    DailyPostモデルにバリデーション追加

--- app/models/daily_post.rb
+++ app/models/daily_post.rb
@@ -2,4 +2,12 @@ class DailyPost < ApplicationRecord
   belongs_to :user
   validates :posted_on, presence: true
   validates :content, presence: true, length: { maximum: 365 }
+  validate :only_one_post_per_day, on: :create
+
+  private
+    def only_one_post_per_day
+      if user && user.daily_posts.where(posted_on: posted_on).exists?
+        errors.add(:base, "今日はすでに投稿済みだよ。")
+      end
+    end
 end
