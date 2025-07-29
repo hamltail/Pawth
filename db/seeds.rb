8af40ff2caabf1b928152451d222d399404c301f
h-waji <goo.hiro.w@gmail.com>
2025-07-29 15:08:25 +0900

    サンプルデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,30 @@
 require "faker"
 
-FactoryBot.create(:user, username: "miku", email: "miku@example.com")
-FactoryBot.create(:user, username: "rin", email: "rin@example.com")
-FactoryBot.create(:user, username: "len", email: "len@example.com")
-FactoryBot.create_list(:user, 5)
+miku = FactoryBot.create(:user, username: "miku", email: "miku@example.com")
+rin = FactoryBot.create(:user, username: "rin", email: "rin@example.com")
+len = FactoryBot.create(:user, username: "len", email: "len@example.com")
+other_users = FactoryBot.create_list(:user, 5)
 
-miku = User.find_by(username: "miku")
-FactoryBot.create(:daily_post, user: miku, posted_on: 3.days.ago.to_date)
+(1..90).each do |n|
+  FactoryBot.create(
+    :daily_post,
+    user: miku,
+    posted_on: n.days.ago.to_date,
+    content: Faker::Lorem.paragraph_by_chars(number: 365),
+    edit_count: rand(0..2)
+  )
+end
+
+target_users = [rin, len] + other_users
+target_users.each do |user|
+  dates = (0..29).to_a.sample(10)
+  dates.each do |days_ago|
+    FactoryBot.create(
+      :daily_post,
+      user: user,
+      posted_on: days_ago.days.ago.to_date,
+      content: Faker::Lorem.paragraph_by_chars(number: 365),
+      edit_count: rand(0..2)
+    )
+  end
+end
