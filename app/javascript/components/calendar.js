73e26077701b93d05d80f4953ed5280255dcf819
h-waji <goo.hiro.w@gmail.com>
2025-07-31 17:28:42 +0900

    カレンダーにgsap導入

--- app/javascript/components/calendar.js
+++ app/javascript/components/calendar.js
@@ -1,10 +1,17 @@
-document.addEventListener("turbo:load", function() {
-  document.querySelectorAll('span[data-date]').forEach(el => {
-    el.addEventListener('click', function() {
-      const date = this.dataset.date;
-      const content = this.dataset.content;
-      document.getElementById('daily-post-date').textContent = date;
-      document.getElementById('daily-post-content').textContent = content || "まだ投稿がありません。";
+import("https://cdn.skypack.dev/gsap").then(({ default: gsap }) => {
+  document.addEventListener("turbo:load", function() {
+    document.querySelectorAll('span[data-date]').forEach(el => {
+      el.addEventListener('click', function() {
+        const date = this.dataset.date;
+        const content = this.dataset.content;
+        document.getElementById('daily-post-date').textContent = date;
+        document.getElementById('daily-post-content').textContent = content || "まだ投稿がありません。";
+
+        gsap.fromTo("#daily-post-content", 
+          { y: -10, opacity: 0 },
+          { y: 0, opacity: 1, duration: 0.5, ease: "power1.out" }
+        );
+      });
     });
   });
 });
