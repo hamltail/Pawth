0379827afd3c0f2cd89efafeb2bb2a93dd601cf8
h-waji <goo.hiro.w@gmail.com>
2025-08-07 13:25:44 +0900

    サイドバーの外をクリックした時に閉じるように修正

--- app/javascript/components/menu.js
+++ app/javascript/components/menu.js
@@ -1,4 +1,4 @@
-window.toggleSidebar = function () {
+window.toggleSidebar = () => {
   const sidebar = document.getElementById("sidebar");
   const menuButton = document.getElementById("menu-button");
   sidebar.classList.toggle("-translate-x-full");
@@ -9,3 +9,19 @@ window.toggleSidebar = function () {
     menuButton.style.display = "none";
   }
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  const sidebar = document.getElementById("sidebar");
+  const menuButton = document.getElementById("menu-button");
+
+  document.addEventListener("click", (e) => {
+    const sidebarVisible = !sidebar.classList.contains("-translate-x-full");
+    const clickedSidebar = sidebar.contains(e.target);
+    const clickedMenuButton = menuButton.contains(e.target);
+
+    if (sidebarVisible && !clickedSidebar && !clickedMenuButton) {
+      sidebar.classList.add("-translate-x-full");
+      menuButton.style.display = "block";
+    }
+  });
+});
