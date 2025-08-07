c9e6b7376bf5fc40e9e8a6b11305fdca2d06fed5
h-waji <goo.hiro.w@gmail.com>
2025-08-07 14:18:03 +0900

    リンククリック時にサイドバーを閉じるよう修正

--- app/javascript/components/menu.js
+++ app/javascript/components/menu.js
@@ -1,27 +1,35 @@
 window.toggleSidebar = () => {
-  const sidebar = document.getElementById("sidebar");
-  const menuButton = document.getElementById("menu-button");
-  sidebar.classList.toggle("-translate-x-full");
+  const sidebar = document.getElementById('sidebar');
+  const menuButton = document.getElementById('menu-button');
+  sidebar.classList.toggle('-translate-x-full');
   
-  if (sidebar.classList.contains("-translate-x-full")) {
-    menuButton.classList.remove("hidden");
+  if (sidebar.classList.contains('-translate-x-full')) {
+    menuButton.classList.remove('hidden');
   } else {
-    menuButton.classList.add("hidden");
+    menuButton.classList.add('hidden');
   }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  const sidebar = document.getElementById("sidebar");
-  const menuButton = document.getElementById("menu-button");
+window.addEventListener('DOMContentLoaded', () => {
+  const sidebar = document.getElementById('sidebar');
+  const menuButton = document.getElementById('menu-button');
 
-  document.addEventListener("click", (e) => {
-    const sidebarVisible = !sidebar.classList.contains("-translate-x-full");
+  document.addEventListener('click', (e) => {
+    const sidebarVisible = !sidebar.classList.contains('-translate-x-full');
     const clickedSidebar = sidebar.contains(e.target);
     const clickedMenuButton = menuButton.contains(e.target);
 
     if (sidebarVisible && !clickedSidebar && !clickedMenuButton) {
-      sidebar.classList.add("-translate-x-full");
-      menuButton.classList.remove("hidden");
+      sidebar.classList.add('-translate-x-full');
+      menuButton.classList.remove('hidden');
     }
+  });
+
+  document.querySelectorAll('#sidebar nav a').forEach(link => {
+    link.addEventListener('click', (e) => {
+      sidebar.classList.add('-translate-x-full');
+      menuButton.classList.remove('hidden');
+      e.stopPropagation();
+    });
   });
 });
