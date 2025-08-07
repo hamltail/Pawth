2e2d91f6fab56a7a4856bb1621c02c2acc581531
h-waji <goo.hiro.w@gmail.com>
2025-08-07 13:13:28 +0900

    サイドバー表示時にハンバーガーメニューを非表示

--- app/javascript/components/menu.js
+++ app/javascript/components/menu.js
@@ -1,4 +1,11 @@
 window.toggleSidebar = function () {
   const sidebar = document.getElementById("sidebar");
+  const menuButton = document.getElementById("menu-button");
   sidebar.classList.toggle("-translate-x-full");
+
+  if (sidebar.classList.contains("-translate-x-full")) {
+    menuButton.style.display = "block";
+  } else {
+    menuButton.style.display = "none";
+  }
 }
