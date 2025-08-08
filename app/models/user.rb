74a35fde223840b34db6746afcecb84d3a22b60d
h-waji <goo.hiro.w@gmail.com>
2025-08-08 12:44:16 +0900

    Deviseのconfirmableを有効化し既存ユーザーを確認済みに設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,8 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_many :daily_posts, dependent: :destroy
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :rememberable, :validatable, :confirmable,
          authentication_keys: [ :login ]
 
   validates :email, presence: true
