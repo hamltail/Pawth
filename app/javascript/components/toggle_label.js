acf341d1f3e7955ca742df1c5e0f37e30f56c593
h-waji <goo.hiro.w@gmail.com>
2025-08-11 15:40:23 +0900

    スタイル調整

--- app/javascript/components/toggle_label.js
+++ app/javascript/components/toggle_label.js
@@ -8,13 +8,13 @@ document.addEventListener("turbo:load", function () {
   function updateLabel() {
     if (checkbox.checked) {
       label.textContent = "公開";
-      label.classList.remove("text-red-700");
-      label.classList.add("text-green-700");
-      gsap.to(bar, { backgroundColor: "#3b82f6", duration: 0.3 });
+      label.classList.remove("text-red-500");
+      label.classList.add("text-green-500");
+      gsap.to(bar, { backgroundColor: "#2dd4bf", duration: 0.3 });
     } else {
       label.textContent = "非公開";
-      label.classList.remove("text-green-700");
-      label.classList.add("text-red-700");
+      label.classList.remove("text-green-500");
+      label.classList.add("text-red-500");
       gsap.to(bar, { backgroundColor: "#d1d5db", duration: 0.3 });
     }
   }
