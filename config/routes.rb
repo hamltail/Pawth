d2640d39b895b4d22ee2ea9bbc10e93fed17973a
h-waji <goo.hiro.w@gmail.com>
2025-08-12 13:08:57 +0900

    ルーティング設定をリファクタ

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    root to: "devise/sessions#new"
+    root "devise/sessions#new"
   end
 
-  resources :daily_posts, only: [ :index, :new, :create, :edit, :update, :destroy ]
+  resources :daily_posts, only: %i[ index new create edit update destroy ]
 
   get "/:username", to: "activities#show", as: :activity
 end
