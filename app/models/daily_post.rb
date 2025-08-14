ce2c3f68583730a359b0906de6c86a67f535de8f
h-waji <goo.hiro.w@gmail.com>
2025-08-14 11:16:20 +0900

    投稿時のバリデーションメッセージとレイアウトを修正

--- app/models/daily_post.rb
+++ app/models/daily_post.rb
@@ -2,8 +2,10 @@ class DailyPost < ApplicationRecord
   EDIT_COUNT_LIMIT = 2
 
   belongs_to :user
+
   validates :posted_on, presence: true
   validates :content, presence: true, length: { maximum: 365 }
+  
   validate :only_one_post_per_day, on: :create
   validate :edit_count_within_limit, on: :update
   validate :only_today_can_be_edited, on: :update
@@ -28,19 +30,19 @@ class DailyPost < ApplicationRecord
   private
     def only_one_post_per_day
       if user && user.daily_posts.where(posted_on: posted_on).exists?
-        errors.add(:base, "今日はすでに投稿済みだよ。")
+        errors.add(:base, "今日はすでに投稿済みです")
       end
     end
 
     def edit_count_within_limit
       if edit_count > EDIT_COUNT_LIMIT
-        errors.add(:base, "編集は#{EDIT_COUNT_LIMIT}回までだよ。")
+        errors.add(:base, "編集は#{EDIT_COUNT_LIMIT}回までです")
       end
     end
 
     def only_today_can_be_edited
       if posted_on != Date.today
-        errors.add(:base, "編集できるのは当日（#{posted_on.strftime('%Y-%m-%d')}）だけだよ。")
+        errors.add(:base, "編集は当日（#{posted_on.strftime('%Y-%m-%d')}）のみ可能です")
       end
     end
 end
