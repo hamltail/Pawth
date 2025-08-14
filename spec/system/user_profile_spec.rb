713a825f57e3674b37c9ec63f6966a4ce064a92e
h-waji <goo.hiro.w@gmail.com>
2025-08-14 12:45:49 +0900

    UI文言を日記アプリ仕様に変更

--- spec/system/user_profile_spec.rb
+++ spec/system/user_profile_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'User Profile Edit', type: :system do
+RSpec.describe 'ユーザー設定', type: :system do
   let(:user) { FactoryBot.create(:user, password: "password123") }
 
-  it 'ログインしてアクティビティ画面が表示できる' do
+  it 'ログインしてカレンダーが表示されること' do
     visit root_path
     fill_in 'user_login', with: user.email
     fill_in 'user_password', with: 'password123'
     click_button 'ログイン'
-    expect(page).to have_content("#{user.username}'s Activity")
+    expect(page).to have_content("#{user.username}さんの日記")
     expect(page).to have_content("ようこそ、#{user.username}さん！")
     expect(page).not_to have_content("非公開です")
   end
