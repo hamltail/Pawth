bf2b834b7604c26bba38986021657b02bcade79c
h-waji <goo.hiro.w@gmail.com>
2025-08-19 17:10:51 +0900

    メール文面を修正 & プレビュー対応

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,27 @@
+# http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+  def confirmation_instructions
+    user = sample_user
+    Devise::Mailer.confirmation_instructions(user, "preview-token")
+  end
+
+  def reset_password_instructions
+    user = sample_user
+    Devise::Mailer.reset_password_instructions(user, "preview-token")
+  end
+
+  def email_changed
+    user = sample_user
+    Devise::Mailer.email_changed(user)
+  end
+
+  def password_change
+    user = sample_user
+    Devise::Mailer.password_change(user)
+  end
+
+  private
+    def sample_user
+      User.first || User.new(email: "preview@example.com", name: "プレビュー用ユーザー")
+    end
+end
