8df3bfc3329089a256719a824917695aa384b037
h-waji <goo.hiro.w@gmail.com>
2025-09-01 13:04:32 +0900

    fade_in_postsをStimulus controllerへ移行

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,6 @@
 import '@hotwired/turbo-rails';
 import 'controllers';
 import 'components/calendar';
-import 'components/fade_in_posts';
 import 'components/menu';
 import 'components/preview_avatar';
 import 'components/toggle_label';
