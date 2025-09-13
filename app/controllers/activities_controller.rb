492d4ea2196cac7964e77b84bbc5c431757570da
h-waji <goo.hiro.w@gmail.com>
2025-09-13 15:09:08 +0900

    カレンダーをリファクタ

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,19 +4,18 @@ class ActivitiesController < ApplicationController
   def show
     @latest_daily_post = @user.daily_posts.order(posted_on: :desc).first
 
-    today = if params[:year].present? && params[:month].present?
+    current_month = if params[:year].present? && params[:month].present?
       Date.new(params[:year].to_i, params[:month].to_i)
     else
       Date.current
     end
 
-    @calendar_days = (today.beginning_of_month..today.end_of_month).to_a
+    @calendar_days = (current_month.beginning_of_month..current_month.end_of_month).to_a
     @daily_posts_by_date = @user.daily_posts
-                                .where(posted_on: today.beginning_of_month..today.end_of_month)
+                                .where(posted_on: current_month.beginning_of_month..current_month.end_of_month)
                                 .group_by(&:posted_on)
-    @prev_month = today.prev_month
-    @next_month = today.next_month
-    @month_label = today.strftime('%B %Y')
+    @prev_month = current_month.prev_month
+    @next_month = current_month.next_month
   end
 
   private
