69142487f5e6bd667418db4d97cf303e63bc94d3
h-waji <goo.hiro.w@gmail.com>
2025-09-25 10:49:46 +0900

    統合テストを追加

---
+++ spec/requests/daily_posts_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'DailyPosts', type: :request do
+  Rails.application.routes_reloader.execute_unless_loaded
+  let(:user) { create(:user) }
+  before { sign_in user }
+
+  it '今日の投稿は削除できない' do
+    post_today = create(:daily_post, user: user, posted_on: Date.current)
+
+    delete daily_post_path(post_today)
+
+    expect(response).to redirect_to(daily_posts_path)
+    follow_redirect!
+    expect(response.body).to include('今日の日記は削除できません。')
+    expect(DailyPost.exists?(post_today.id)).to be(true)
+  end
+end
