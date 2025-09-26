15316c96dd721585444c0504973d129b42d04595
h-waji <goo.hiro.w@gmail.com>
2025-09-26 12:19:58 +0900

    e2eにテストコードを追加

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -1,6 +1,21 @@
 import { test, expect } from '@playwright/test';
+import { execSync } from 'node:child_process';
 
-test('トップページが開ける', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveURL(/\/$/);
+test.beforeAll(() => {
+  execSync('RAILS_ENV=test ../bin/rails e2e:ensure_user', {
+    stdio: 'inherit',
+    cwd: __dirname + '/..',
+  });
+});
+test('ログインしてログアウトできる', async ({ page }) => {
+  await page.goto('/users/sign_in');
+  await page
+    .getByRole('textbox', { name: 'ユーザー名またはメールアドレス' })
+    .fill('playwright@example.com');
+  await page.getByRole('textbox', { name: 'パスワード' })
+    .fill('password1234');
+  await page.getByRole('button', { name: 'ログイン' }).click();
+  await expect(page.getByRole('link', { name: 'Pawth' })).toBeVisible();
+  await page.getByRole('link', { name: 'ログアウト' }).click();
+  await expect(page.getByText('ログアウトしました。')).toBeVisible();
 });
