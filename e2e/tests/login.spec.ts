9b02dd9b378ac7f2a9639d30fc0cbd4a0252c471
h-waji <goo.hiro.w@gmail.com>
2025-09-26 12:32:05 +0900

    ci.yml に e2e(Playwright) を追加

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -12,8 +12,7 @@ test('ログインしてログアウトできる', async ({ page }) => {
   await page
     .getByRole('textbox', { name: 'ユーザー名またはメールアドレス' })
     .fill('playwright@example.com');
-  await page.getByRole('textbox', { name: 'パスワード' })
-    .fill('password1234');
+  await page.getByRole('textbox', { name: 'パスワード' }).fill('password1234');
   await page.getByRole('button', { name: 'ログイン' }).click();
   await expect(page.getByRole('link', { name: 'Pawth' })).toBeVisible();
   await page.getByRole('link', { name: 'ログアウト' }).click();
