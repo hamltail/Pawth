37d831ba973faa70a376b6f8182bc4747f3973d1
h-waji <goo.hiro.w@gmail.com>
2025-10-03 13:49:00 +0900

    ActivitiesControllerをリファクタ

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,26 +1,43 @@
 class ActivitiesController < ApplicationController
   before_action :set_user
+  before_action :set_month
 
   def show
     @current_post = @user.daily_posts.order(posted_on: :desc).first
 
-    current_month = if params[:year].present? && params[:month].present?
-      Date.new(params[:year].to_i, params[:month].to_i)
-    else
-      Date.current
-    end
+    month_range = @month.all_month
+    posts       = @user.daily_posts.where(posted_on: month_range).to_a
 
-    @calendar_days = (current_month.beginning_of_month..current_month.end_of_month).to_a
-    @daily_posts_by_date = @user.daily_posts
-                                .where(posted_on: current_month.beginning_of_month..current_month.end_of_month)
-                                .group_by(&:posted_on)
-    @prev_month = current_month.prev_month
-    @next_month = current_month.next_month
+    @calendar_days       = month_range.to_a
+    @daily_posts_by_date = posts.index_by(&:posted_on)
+
+    @prev_month = @month.prev_month
+    @next_month = @month.next_month
   end
 
   private
 
   def set_user
     @user = User.find_by!(username: params[:username])
+  end
+
+  def set_month
+    y = safe_int(params[:year])
+    m = safe_int(params[:month])
+
+    @month =
+      if y && m && (1..12).include?(m)
+        Date.new(y, m)
+      else
+        Date.current
+      end
+  rescue ArgumentError
+    @month = Date.current
+  end
+
+  def safe_int(v)
+    Integer(v) if v.present?
+  rescue ArgumentError, TypeError
+    nil
   end
 end
