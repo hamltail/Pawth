b2c95646a0018f1d03745a9426ff65ff0e9d4af8
h-waji <goo.hiro.w@gmail.com>
2025-10-03 14:09:18 +0900

    ApplicationControllerをリファクタ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
-  after_action :log_flash_notice_in_dev
+
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action  :log_flash_notice_in_dev
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   def after_sign_in_path_for(resource)
-    activity_path(resource.username)
+    stored_location_for(resource) || activity_path(resource.username)
   end
 
-  def after_sign_out_path_for(resource)
+  def after_sign_out_path_for(_resource)
     new_user_session_path
   end
 
@@ -17,21 +18,25 @@ class ApplicationController < ActionController::Base
 
   def log_flash_notice_in_dev
     return unless Rails.env.development?
-    Rails.logger.info("[FLASH] notice=#{flash[:notice].inspect}") if flash[:notice].present?
-    Rails.logger.info("[FLASH] alert=#{flash[:alert].inspect}") if flash[:alert].present?
+    payload = flash.to_hash.slice('notice', 'alert').compact_blank
+    return if payload.empty?
+    Rails.logger.tagged('FLASH') { Rails.logger.info(payload.inspect) }
   end
 
   def render_404
     respond_to do |f|
-      f.html { render file: Rails.root.join('public/404.html'), status: :not_found, layout: false }
+      f.html { render file: Rails.public_path.join('404.html'), status: :not_found, layout: false }
+      f.turbo_stream { head :not_found }
       f.any { head :not_found }
     end
   end
 
   protected
 
+  ALLOWED_KEYS = %i[username email].freeze
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: ALLOWED_KEYS)
+    devise_parameter_sanitizer.permit(:account_update, keys: ALLOWED_KEYS)
   end
 end
