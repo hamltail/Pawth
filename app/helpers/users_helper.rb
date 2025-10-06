044644756df4785211087541f9da9a7bb3a54472
h-waji <goo.hiro.w@gmail.com>
2025-10-06 16:11:04 +0900

    UsersHelperをリファクタ

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,13 @@
 module UsersHelper
   def avatar_for(user, size: 80, css: '')
-    if user&.profile&.avatar&.attached?
-      image_tag user.profile.avatar,
-                width: size, height: size,
-                class: "rounded-full object-cover #{css}",
-                alt: t('helpers.avatar.user', username: user.username)
-    else
-      image_tag 'default_avatar.png',
-                size: "#{size}x#{size}",
-                class: "rounded-full object-cover #{css}",
-                alt: t('helpers.avatar.default')
-    end
+    avatar = user&.profile&.avatar
+    alt_text = user&.username ? t('helpers.avatar.user', username: user.username) : t('helpers.avatar.default')
+
+    image_tag(
+      avatar&.attached? ? avatar : 'default_avatar.png',
+      width: size, height: size,
+      class: "rounded-full object-cover #{css}",
+      alt: alt_text
+    )
   end
 end
