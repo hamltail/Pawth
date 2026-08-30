866777f2d5491aaeddd2e20c59a59cb9b48836d0
h-waji <goo.hiro.w@gmail.com>
2026-08-30 19:44:55 +0900

    カレンダーの表示変更に合わせてシステムテストを修正

--- spec/system/user_profile_spec.rb
+++ spec/system/user_profile_spec.rb
@@ -8,8 +8,9 @@ RSpec.describe 'ユーザー設定', type: :system do
     fill_in 'user_login', with: user.email
     fill_in 'user_password', with: 'password123'
     click_button 'ログイン'
-    expect(page).to have_content("#{user.username}さんの日記")
-    expect(page).to have_content("ようこそ、#{user.username}さん！")
-    expect(page).not_to have_content("非公開です。")
+
+    expect(page).to have_content("#{user.username}'s Pawth")
+    expect(page).to have_content('YOUR DAILY PAWTH')
+    expect(page).not_to have_content('非公開です。')
   end
 end
