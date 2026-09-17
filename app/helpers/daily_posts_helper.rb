c9fc179a5ebdb0e02919b89aa8f70a945bd04e1b
h-waji <goo.hiro.w@gmail.com>
2026-09-17 13:48:09 +0900

    日記一覧をタイムライン型のUIに改善

--- app/helpers/daily_posts_helper.rb
+++ app/helpers/daily_posts_helper.rb
@@ -1,3 +1,18 @@
 module DailyPostsHelper
   include Pagy::Frontend
+
+  def timeline_mark_for(icon: :paw)
+    render calendar_mark_partial(icon),
+           klass: '!w-8 !h-8',
+           dataset: {},
+           grad: true
+  end
+
+  def timeline_month_label(date)
+    date.strftime(t('date.formats.month_year'))
+  end
+
+  def timeline_weekday_label(date)
+    I18n.t('date.abbr_day_names', locale: :en)[date.wday]
+  end
 end
