7756ace9ec9a21fee44846d0edad4baa8985325e
h-waji <goo.hiro.w@gmail.com>
2026-09-17 11:32:41 +0900

    カレンダーに星アイコンを追加

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -36,16 +36,28 @@ module CalendarHelper
     date == today
   end
 
+  # def calendar_mark_for(date, post)
+  #   if post
+  #     render 'shared/paw',
+  #            klass: 'text-pink-300 cursor-pointer calendar-mark--posted',
+  #            dataset: { date:, content: post.content },
+  #            grad: true
+  #   else
+  #     render 'shared/paw',
+  #            klass: 'text-muted opacity-50',
+  #            grad: false
+  #   end
+  # end
   def calendar_mark_for(date, post)
     if post
-      render 'shared/paw',
-             klass: 'text-pink-300 cursor-pointer calendar-mark--posted',
-             dataset: { date:, content: post.content },
-             grad: true
+      render 'shared/star',
+            klass: 'text-yellow-400 cursor-pointer calendar-mark--posted',
+            dataset: { date:, content: post.content },
+            grad: true
     else
-      render 'shared/paw',
-             klass: 'text-muted opacity-50',
-             grad: false
+      render 'shared/star',
+            klass: 'text-muted opacity-50',
+            grad: false
     end
   end
 
